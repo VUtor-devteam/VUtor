6ba03c4de2978ed2162194e66c583ad4722d1725
Ulomei <Gab.Drungilaite@gmail.com>
2023-11-23 19:00:17 +0200

    Added other needed files for geting the ratings to database

---
+++ DataAccessLibrary/RatingRepository.cs
@@ -0,0 +1,52 @@
+﻿using DataAccessLibrary.Data;
+using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary
+{
+    public class RatingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RatingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddOrUpdateRatingAsync(Rating rating)
+        {
+            var existingRating = await _context.Ratings
+                .FirstOrDefaultAsync(r => r.RecipientId == rating.RecipientId && r.ReviewerId == rating.ReviewerId);
+
+            if (existingRating != null)
+            {
+                existingRating.Score = rating.Score;
+            }
+            else
+            {
+                _context.Ratings.Add(rating);
+            }
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<double> GetAverageRatingAsync(string recipientId)
+        {
+            var ratings = await _context.Ratings
+                                        .Where(r => r.RecipientId == recipientId)
+                                        .ToListAsync();
+
+            if (!ratings.Any())
+            {
+                return 0; // Return 0 or some default value if there are no ratings
+            }
+
+            return ratings.Average(r => r.Score);
+        }
+    }
+}
